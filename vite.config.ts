62dab50296c847538721b3b8acfb5414953e2de7
Etienne M <em85@pm.me>
2026-01-06 23:42:37 +0000

    feat: add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+    // Load env file based on mode in the current working directory
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        base: `/${env.VITE_PROJECT_NAME}/`,
+        plugins: [react(), tailwindcss()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        server: {
+            allowedHosts: env.VITE_LOCAL_DEPLOYMENT_URLS?.split(',').map((host) => host.trim()) || [],
+        },
+    }
 })
